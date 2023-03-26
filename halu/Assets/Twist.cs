9f904841bf6c2d6603acbce9b046d58cc16a393e
halu990716 <126733230+halu990716@users.noreply.github.com>
2023-03-27 07:54:55 +0900

    Update Twist.cs

--- halu/Assets/Twist.cs
+++ halu/Assets/Twist.cs
@@ -6,46 +6,58 @@ public class Twist : MonoBehaviour
 {
     public float Angle;
     public float Speed;
+    public Sprite sprite;
 
     private GameObject[] Bullet = new GameObject[2];
+
     private void Start()
     {
         GameObject ParentObj = new GameObject("Twist");
 
+        SpriteRenderer renderer = ParentObj.AddComponent<SpriteRenderer>();
+
+        renderer.sprite = sprite;
+
 
         Angle = 0.0f;
         Speed = 5.0f;
-
+             
         Bullet[0] = new GameObject("TwistBullet");
+
+        Bullet[0].transform.SetParent(transform);
 
         Bullet[0].AddComponent<MyGizmo>();
 
         Bullet[0].transform.position = new Vector3(
             transform.position.x,
-            transform.position.y + 2.5f,
+            transform.position.y - 1.25f,
             0.0f);
 
 
         Bullet[1] = new GameObject("TwistBullet");
 
+        Bullet[1].transform.SetParent(transform);
+
         Bullet[1].AddComponent<MyGizmo>();
 
         Bullet[1].transform.position = new Vector3(
             transform.position.x,
-            transform.position.y - 2.5f,
+            transform.position.y + 1.25f,
             0.0f);
     }
 
     void Update()
     {
-        Angle += 0.5f;
+        Angle += 1.0f;
 
-        Bullet[0].transform.position = new Vector3(
-            1.0f,
-            Mathf.Sin(Angle * Mathf.Deg2Rad), 0.0f);
+        Bullet[0].transform.position += new Vector3(
+                1.0f,
+                Mathf.Sin(Angle * Mathf.Deg2Rad),
+                0.0f) * Speed * Time.deltaTime;
 
-        Bullet[1].transform.position = new Vector3(
-            Mathf.Sin(-Angle * Mathf.Deg2Rad), 1.0f, 0.0f);
-
+        Bullet[1].transform.position += new Vector3(
+                1.0f,
+                Mathf.Sin(-Angle * Mathf.Deg2Rad),
+                0.0f) * Speed * Time.deltaTime;
     }
 }
